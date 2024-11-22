c30b9e5bf6c855063ba44b805fbd66ab7f25174f
Albert Chambers <albertchambers09@gmail.com>
2024-11-22 08:53:47 +0200

    create ErrorMessage component, export ErrorMessage

---
+++ components/ErrorMessage.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+/**
+ * Component to display an error message.
+ * @param {string} message - Error message to display.
+ * @returns {JSX.Element} A rendered error message.
+ */
+const ErrorMessage = ({ message }) => (
+  <div style={{ color: 'red' }}>
+    <p>Error: {message}</p>
+  </div>
+);
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default ErrorMessage;
