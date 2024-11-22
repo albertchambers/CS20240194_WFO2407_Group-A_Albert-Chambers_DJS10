a410030e45ac6ec80cfe0f022a1f36c0c972a402
Albert Chambers <albertchambers09@gmail.com>
2024-11-22 08:56:36 +0200

    create api service

---
+++ services/api.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * Fetch posts from the JSONPlaceholder API.
+ * @returns {Promise<object[]>} A promise resolving to a list of posts.
+ * @throws Will throw an error if the API response is not "ok".
+ */
+export const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch posts: ${response.statusText}`);
+  }
+
+  return response.json();
+};
